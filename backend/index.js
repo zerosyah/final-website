fd12fb171c88755cf8ce331aa7139a5c598e25d0
Siyabonga Shezi <154099964+zerosyah@users.noreply.github.com>
2024-01-11 15:36:14 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,13 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require('path')
 const app = express();
-const __dirname = path.resolve()
+const __dirnames = path.resolve()
 
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirnames, 'client/dist')));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'client','dist', 'index.html'));
+  res.sendFile(path.join(__dirnames, 'client','dist', 'index.html'));
 })
 app.use(cookieParser());
 app.use(cors());

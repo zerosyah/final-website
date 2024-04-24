2a3b4cb5937308224655ddec129e96c6bd811ec3
Siyabonga Shezi <154099964+zerosyah@users.noreply.github.com>
2024-04-25 01:32:29 +0200

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -70,7 +70,7 @@ export default function signin() {
                   // navigate to user info form
                   navigate("/register")
                 }else{
-                    navigate("/dashboard?tab=dash");
+                    navigate("/dashboard?tab=profile");
                 }
       }
     } catch (error) {
